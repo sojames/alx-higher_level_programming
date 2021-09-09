e30b4cb18230df714eb517d8b76d917ad324f5ca
sojames <wokteche@gmail.com>
2021-09-09 13:55:19 -0700

    debugging

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    file = dir(hidden_4)
-    length = len(file)
-    for i in range(0, length):
-        if file[i][0:2] != "__
+    import hidden_4
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print("{:s}".format(name))
