efa81b2f4e81e37ab0396a81944d727c8b2f7f18
sojames <wokteche@gmail.com>
2021-09-09 13:49:12 -0700

    please import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
-def main():
-    a = 1
-    b = 2
-    c = add(a, b)
-    print("{} + {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    from add_0 import add
+    def main():
+        a = 1
+        b = 2
+        c = add(a, b)
+        print("{} + {} = {}".format(a, b, c))
 
 if __name__ == "__main__":
     main()
