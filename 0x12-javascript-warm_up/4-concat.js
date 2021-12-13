0094afd2baea681b9602ccc98aff216156632aaa
sojames <wokteche@gmail.com>
2021-12-13 14:51:15 -0800

    hello javascript

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+let myVar1 = process.argv[2];
+let myBar2 = process.argv[3];
+console.log(myVar1 + " is " myVar2);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,3 @@
+#0x12. JavaScript - Warm up
+## Am back with javascript
+
